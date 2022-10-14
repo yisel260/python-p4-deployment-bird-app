f685081020c58237e1a1074c0127631f1a5d175c
Ben Botsford <ben.botsford@flatironschool.com>
2022-10-14 11:30:26 -0500

    url to uri in sql

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-#!/usr/bin/env python3
-
 import os
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
 from models import db, Bird
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
@@ -24,20 +22,6 @@ class Birds(Resource):
         birds = [bird.to_dict() for bird in Bird.query.all()]
         return make_response(jsonify(birds), 200)
 
-    def post(self):
-
-        data = request.get_json()
-
-        new_bird = Bird(
-            name=data['name'],
-            species=data['species'],
-        )
-
-        db.session.add(new_bird)
-        db.session.commit()
-
-        return make_response(new_bird.to_dict(), 201)
-
 api.add_resource(Birds, '/birds')
 
 class BirdByID(Resource):
@@ -45,30 +29,4 @@ class BirdByID(Resource):
         bird = Bird.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(bird), 200)
 
-    def patch(self, id):
-
-        data = request.get_json()
-
-        bird = Bird.query.filter_by(id=id).first()
-
-        for attr in data:
-            setattr(bird, attr, data[attr])
-
-        db.session.add(bird)
-        db.session.commit()
-
-        return make_response(bird.to_dict(), 200)
-
-    def delete(self, id):
-
-        bird = Bird.query.filter_by(id=id).first()
-        db.session.delete(bird)
-        db.session.commit()
-
-        return make_response('', 204)
-
 api.add_resource(BirdByID, '/birds/<int:id>')
-
-
-if __name__ == '__main__':
-    app.run(port=5555)

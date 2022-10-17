1f7e906d2bdab8977a96639238c126b04777b966
Ben Botsford <ben.botsford@flatironschool.com>
2022-10-17 11:11:48 -0500

    add .env

--- server/app.py
+++ server/app.py
@@ -1,5 +1,8 @@
 import os
 
+from dotenv import load_dotenv
+load_dotenv()
+
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
